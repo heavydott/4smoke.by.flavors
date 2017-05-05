b89c2ac0ca0f2a1accbcc602bde6186fea80909e
heavydott <shustart@gmail.com>
2017-05-05 16:12:29 +0200

    load bug fixes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(function(flavors4Smoke) {
 	var $prods = $('.prods');
 	var $flavors = $('.flavors');
 
+	// Products makers
 	var prodsItems = '';
 
 	for(var i = 0; i < flavors4Smoke.length; i++) {
@@ -15,6 +16,30 @@ $(function(flavors4Smoke) {
 
 	$prods.html(prodsItems);
 
+	// Flavors
+	var flavorsItems = '<button>назад</button>';
+
+	for(var i = 0; i < flavors4Smoke.length; i++) {
+		flavorsItems += '<div class="flav-cont ' + flavors4Smoke[i].hash + '">';
+
+		for(var j = 0; j < flavors4Smoke[i].flavors.length; j++) {
+			flavorsItems += '<div class="flavor-item">' + 
+								'<p class="title">'+ flavors4Smoke[i].name + ' - ' + flavors4Smoke[i].flavors[j].name + '</p>' +
+								'<img src="' + flavors4Smoke[i].url + flavors4Smoke[i].flavors[j].img + '.png" alt="" />' +
+								'<p class="flavor">' + flavors4Smoke[i].flavors[j].flavor + '</p>' +
+								'<p class="desc">' + flavors4Smoke[i].flavors[j].desc + '</p>' +
+							'</div>';
+		}
+
+		flavorsItems += '<div style="clear: both;"></div></div>';
+
+		$flavors.html(flavorsItems);
+
+		$('.flavors button').on('click', function() {
+			window.location.hash = '#';
+		});
+	}
+
 	function hashchange() {
 		var hash = window.location.hash.substr(1);
 
@@ -27,26 +52,9 @@ $(function(flavors4Smoke) {
 
 			for(var i = 0; i < flavors4Smoke.length; i++) {
 				if(flavors4Smoke[i].hash == hash) {
-					var flavorsItems = '';
-
-					flavorsItems += '<button>назад</button>';
-
-					for(var j = 0; j < flavors4Smoke[i].flavors.length; j++) {
-						flavorsItems += '<div class="flavor-item">' + 
-											'<p class="title">'+ flavors4Smoke[i].name + ' - ' + flavors4Smoke[i].flavors[j].name + '</p>' +
-											'<img src="' + flavors4Smoke[i].url + flavors4Smoke[i].flavors[j].img + '.png" alt="" />' +
-											'<p class="flavor">' + flavors4Smoke[i].flavors[j].flavor + '</p>' +
-											'<p class="desc">' + flavors4Smoke[i].flavors[j].desc + '</p>' +
-										'</div>';
-					}
-
-					flavorsItems += '<div style="clear: both;"></div>';
-
-					$flavors.html(flavorsItems);
-
-					$('.flavors button').on('click', function() {
-						window.location.hash = '#';
-					});
+					$('.flav-cont.' + flavors4Smoke[i].hash).addClass('active');
+				} else {
+					$('.flav-cont.' + flavors4Smoke[i].hash).removeClass('active');
 				}
 			}
 		}
